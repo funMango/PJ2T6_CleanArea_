e8b01464bb24ba120179ad854f3515f116f22050
funMango <138420832+funMango@users.noreply.github.com>
2023-12-07 10:09:50 +0900

    start project

---
+++ ClenaArea/ClenaArea/ClenaAreaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClenaAreaApp.swift
+//  ClenaArea
+//
+//  Created by 이민호 on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ClenaAreaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
